83b89ce16e665f9aed7cbd654cdf8eec3c16ee38
Paul-VS-prog <74456837+Paul-VS-prog@users.noreply.github.com>
2022-06-23 20:23:42 +0300

    Added HTTP method GET: get employee by id

--- src/main/java/com/ryumin/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/ryumin/spring/rest/controller/MyRESTController.java
@@ -4,6 +4,7 @@ import com.ryumin.spring.rest.entity.Employee;
 import com.ryumin.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,12 @@ public class MyRESTController {
     public List<Employee> showAllEmployees() {
         List<Employee> allEmployee = employeeService.getAllEmployees();
         return allEmployee;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee showEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        return employee;
     }
 
 }
