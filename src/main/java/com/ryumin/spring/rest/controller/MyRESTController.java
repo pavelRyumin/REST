5bab2c14f76fedf42897dc0a43e1dc8e72c42cc1
Paul-VS-prog <74456837+Paul-VS-prog@users.noreply.github.com>
2022-06-24 00:29:36 +0300

    Added Http PUT: update employee

--- src/main/java/com/ryumin/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/ryumin/spring/rest/controller/MyRESTController.java
@@ -34,4 +34,10 @@ public class MyRESTController {
         employeeService.saveEmployee(employee);
         return employee;
     }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
 }
