fe7e5901e7c460b420c1af2c5fc94d322c409b50
Paul-VS-prog <74456837+Paul-VS-prog@users.noreply.github.com>
2022-06-23 21:27:13 +0300

    Global exception handler

--- src/main/java/com/ryumin/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/ryumin/spring/rest/controller/MyRESTController.java
@@ -1,12 +1,9 @@
 package com.ryumin.spring.rest.controller;
 
 import com.ryumin.spring.rest.entity.Employee;
-import com.ryumin.spring.rest.exception_handling.EmployeeIncorrectData;
 import com.ryumin.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.ryumin.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,21 +27,5 @@ public class MyRESTController {
             throw new NoSuchEmployeeException("Employee with id = " + id + " no found");
         }
         return employee;
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 }
